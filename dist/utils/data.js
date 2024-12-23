864b6ac57e6bf9d152462648955485b53d67dc03
Nic Nic Miller <nicmiller13@gmail.com>
2024-12-22 20:33:59 -0500

    deleted seed file

--- dist/utils/data.js
+++ dist/utils/data.js
@@ -1,24 +1,23 @@
 const names = [
     'Aayan',
-  'Aazaan',
-  'Abaan',
-  'Abbas',
-  'Abdallah',
-  'Abdalroof',
-  'Abdihakim',
-  'Abdirahman',
-  'Abdisalam',
-  'Abdul',
-  'Abdul-Aziz',
-  'Abdulbasir',
-  'Abdulkadir',
-  'Abdulkarem',
-  'Smith',
-  'Jones',
-  'Coollastname',
-  'enter_name_here',
+    'Aazaan',
+    'Abaan',
+    'Abbas',
+    'Abdallah',
+    'Abdalroof',
+    'Abdihakim',
+    'Abdirahman',
+    'Abdisalam',
+    'Abdul',
+    'Abdul-Aziz',
+    'Abdulbasir',
+    'Abdulkadir',
+    'Abdulkarem',
+    'Smith',
+    'Jones',
+    'Coollastname',
+    'enter_name_here',
 ];
-
 const thoughts = [
     'I love the way the air smells after it rains.',
     'I think I might be addicted to coffee.',
@@ -27,9 +26,8 @@ const thoughts = [
     'I am so grateful for my friends and family.',
     'How can I measure my progress in becoming a developer beyond technical skills?',
     'How can I challenge myself by learning a new framework or API this month?',
-'How has my perspective on development changed since starting the bootcamp?',
+    'How has my perspective on development changed since starting the bootcamp?',
 ];
-
 const friends = [
     'John',
     'Jane',
@@ -50,9 +48,8 @@ const friends = [
     'Julius',
     'Jude',
 ];
-
-const reactions =[
-     "Thank you so much!",
+const reactions = [
+    "Thank you so much!",
     "I appreciate your help!",
     "That's a great idea!",
     "I love that!",
@@ -63,16 +60,14 @@ const reactions =[
     "I'm so glad you're here!",
     "You're the best!",
     "I'm so grateful for you!",
-]
+];
 //gets a random array item
-const getRandomArrItem = (arr: any[]) => arr[Math.floor(Math.random() * arr.length)];
-
+const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
 //funtion for getting a random name
 const getRandomName = () => {
     return `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
 };
-
-const getRandomThought = (init: number) => {
+const getRandomThought = (init) => {
     let results = [];
     for (let i = 0; i < init; i++) {
         results.push({
@@ -82,10 +77,8 @@ const getRandomThought = (init: number) => {
     }
     return results;
 };
-
 //function for getting random friends
-
-const getRandomFriends = (init: number) => {
+const getRandomFriends = (init) => {
     let results = [];
     for (let i = 0; i < init; i++) {
         results.push({
@@ -94,9 +87,8 @@ const getRandomFriends = (init: number) => {
     }
     return results;
 };
-
 //function for getting random reactions
-const getRandomReactions = (init: number) => {
+const getRandomReactions = (init) => {
     let results = [];
     for (let i = 0; i < init; i++) {
         results.push({
@@ -106,4 +98,4 @@ const getRandomReactions = (init: number) => {
     }
     return results;
 };
-
+export { getRandomName, getRandomThought, getRandomFriends, getRandomReactions };
